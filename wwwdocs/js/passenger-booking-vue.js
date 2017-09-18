e3d7f7d3c482eb8338564ad2d17687d28bdd17db
jsaussy <jaredsaussy@gmail.com>
2017-09-18 00:00:37 -0400

    start converting main interface to vue

---
+++ wwwdocs/js/passenger-booking-vue.js
@@ -0,0 +1,42 @@
+Vue.component('passenger-booking', {
+    template: '<div>\
+    <div class="date-and-id" :id="\'date-and-id\' + idx">\
+      <span class="display-date">{{booking.displayDate}}</span>\
+      <span class="date">{{booking.date}}</span>\
+    </div>\
+    <div class="ready-time-gage">{{booking.displayReadyTime}}</div>\
+    <div class="gauge-container">\
+      <div id="g1" class="gauge"></div>\
+    </div>\
+    <span class="ready-time"><b>Start Window</b>: {{booking.displayReadyTime}}</span>\
+    <span class="ready-time"><b>End Window</b>: {{booking.displayEndWindow}}</span>\
+        <pizza-tracker></pizza-tracker>\
+    <div class="booking-status"><b>Trip Status</b>: {{booking.Status}}<span class="late-status"></span></div>\
+    <div class="times">\
+      <center>\
+        <div class="eta" v-if="booking.displayEta">\
+         <i class="fa fa-clock-o fa-lg"></i> <b>ETA</b>: {{booking.displayEta}}\
+        </div>\
+        <div v-else>\
+            No van is scheduled  for this trip. \
+        </div>\
+        <!-- Not using this right now while we are just displaying one trip.\
+        <span :id="\'details\' + idx" class="more-details"><i class="fa fa-chevron-down" aria-hidden="true"></i> -->\
+        </span>\
+      </center>\
+    </div>\
+    <div class="locations" :id="\'locations\' + idx">\
+      <span class="booking-id"><b>Booking ID</b>: {{booking.bookingID}}</span>\
+      <div class="pickup"><b>From</b>: {{booking.pickupAddress}}</div>\
+      <div class="dropoff"><b>To</b>: {{booking.dropOffAddress}}</div>\
+    </div>\
+  </div> \
+  ',
+  props: ['booking', 'idx'],
+  data: function() {
+      return {}
+  },
+  methods: {
+
+  }
+});
---
+++ wwwdocs/js/pizza-tracker-vue.js
@@ -0,0 +1,35 @@
+Vue.component({
+    template: '\
+    <div class="pizza-tracker">\
+    <div class="booked future-step" data-text="">\
+      <div class="straighten"><i class="fa fa-calendar fa-lg"></i>\
+      </div>\
+    </div>\
+    <div class="scheduled future-step" data-text="">\
+      <div class="straighten"><i class="fa fa-ticket fa-lg"></i>\
+      </div>\
+    </div>\
+    <div class="trip-soon future-step" data-text="">\
+      <div class="straighten"><i class="fa fa-road fa-lg"></i>\
+      </div>\
+    </div>\
+    <div class="five-minutes future-step" data-text="">\
+      <div class="straighten"><i class="fa fa-clock-o fa-lg"></i>\
+      </div>\
+    </div>\
+    <div class="here-now future-step" data-text="">\
+      <div class="straighten">\
+        <div class="bell"><i class="fa fa-bell-o fa-lg"></i></div>\
+      </div>\
+    </div>\
+  </div>\
+    ',
+    data: function() {
+        return {};
+    },
+    methods: {
+        markStepComplete: function(step) {
+            
+        }
+    }
+});
