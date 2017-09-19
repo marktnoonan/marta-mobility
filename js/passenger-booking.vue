2686d60fd22c7b8da3f84148b672491a9b7de739
jsaussy <jaredsaussy@gmail.com>
2017-09-19 00:50:42 -0400

    fix pizza tracker, start converting gauge to vue

--- js/passenger-booking.vue
+++ js/passenger-booking.vue
@@ -1,33 +1,28 @@
 <template>
-  <div>
+  <div class="container booking">
     <div class="date-and-id" :id="'date-and-id' + idx">
       <span class="display-date">{{booking.displayDate}}</span>
       <span class="date">{{booking.date}}</span>
     </div>
     <div class="ready-time-gage">{{booking.displayReadyTime}}</div>
-    <div class="gauge-container">
-      <div id="g1" class="gauge"></div>
-    </div>
+    <passenger-gauge :time="booking.eta" :min-time="earlyTime" :max-time="booking.displayEndWindow" :delay="delay" :new-eta="newEta"></passenger-gauge>
     <span class="ready-time">
       <b>Start Window</b>: {{booking.displayReadyTime}}</span>
     <span class="ready-time">
       <b>End Window</b>: {{booking.displayEndWindow}}</span>
-    <pizza-tracker></pizza-tracker>
+    <pizza-tracker :completed-steps="steps"></pizza-tracker>
     <div class="booking-status">
-      <b>Trip Status</b>: {{booking.Status}}
-      <span class="late-status"></span>
+      <b>Trip Status</b>: {{this.booking.Status}}<span class="late-status">{{lateStatusText}}</span>
     </div>
     <div class="times">
       <center>
-        <div class="eta" v-if="booking.displayEta">
+        <div class="eta" v-if="booking.displayEta" :class="{'no-eta': booking.eta}">
           <i class="fa fa-clock-o fa-lg"></i>
           <b>ETA</b>: {{booking.displayEta}}
         </div>
         <div v-else>
           No van is scheduled for this trip.
         </div>
-        <!-- Not using this right now while we are just displaying one trip.
-          <span :id="'details' + idx" class="more-details"><i class="fa fa-chevron-down" aria-hidden="true"></i> -->
         </span>
       </center>
     </div>
@@ -44,17 +39,154 @@
 
 <script>
 import PizzaTracker from './pizza-tracker.vue';
+import PassengerGauge from './passenger-gauge.vue';
 
 export default {
   components: {
-    PizzaTracker
+    PizzaTracker,
+    PassengerGauge
   },
   props: ['booking', 'idx'],
-  data: function() {
-    return {}
+  computed: {
+    windowEndInMinutes: function() {
+      return this.convertTimeToMinutes(this.booking.endWindow);
+    },
+    earlyTime: function() {
+      return this.convertTimeFromMinutes(this.windowEndInMinutes - 60);
+    },
+    delay: function() {
+      return 30 - (this.windowEndInMinutes - this.convertTimeToMinutes(this.booking.eta));
+    },
+    newEta: function() {
+      return this.booking.eta;
+    },
+    steps: function() {
+      var ret = {};
+      ret.booked = true;
+      var now = new Date();
+      var nowInMinutes = (now.getHours() * 60) + now.getMinutes();
+
+      if (!this.booking.eta) {
+        return;
+      }
+
+      ret.scheduled = true;
+
+      var timeFromNow = this.convertTimeToMinutes(this.booking.eta) - nowInMinutes;
+
+      if (timeFromNow <= 5 && timeFromNow > 0) {
+        ret.five_minutes = true;
+      }
+
+      if (timeFromNow <= 30 && timeFromNow > 0) {
+        ret.trip_soon = true;
+      }
+
+      if (timeFromNow <= 0 && timeFromNow > -10) {
+        ret.here_now = true;
+      }
+      return ret;
+    },
+    lateStatusText: function() {
+      var ret = '';
+      var now = new Date();
+      var nowInMinutes = (now.getHours() * 60) + now.getMinutes();
+      var theEtaInMinutes = this.convertTimeToMinutes(this.booking.eta);
+
+      if (theEtaInMinutes > this.windowEndInMinutes) {
+        ret = ", running late.";
+      } else if (theEtaInMinutes <= this.windowEndInMinutes && this.windowEndInMinutes - theEtaInMinutes < 30) {
+        ret = ", arriving in window.";
+      } else {
+        ret = ", arriving on time.";
+      }
+      return ret;
+    }
   },
   methods: {
+    convertTimeToMinutes: function(time) {
+      var timeInMinutes = time.split(":");
+      timeInMinutes = (timeInMinutes[0] * 60) + parseInt(timeInMinutes[1]);
+      return timeInMinutes;
+    },
+    convertTimeFromMinutes: function(minutes) {
+      var minuteSegment = minutes % 60;
+      //adding the leading zero to minute if needed.
+      minuteSegment = minuteSegment < 10 ? "0" + minuteSegment : minuteSegment;
+      var hourSegment = (minutes - minuteSegment) / 60;
+      var amPm = hourSegment < 12 ? "AM" : "PM";
+      // converting from Military time if needed.
+      var convertedHour = hourSegment === 12 ? hourSegment : hourSegment % 12;
+      // adding leading zero to hour if hour is 0.
+      convertedHour = convertedHour === 0 ? "0" + convertedHour : convertedHour;
+      return convertedHour + ":" + minuteSegment + " " + amPm;
+    }
 
   }
 }
 </script>
+
+
+<style scoped>
+.booking {
+  background-color: #ffffff;
+  margin-bottom: 20px;
+  padding-bottom: 0;
+  border: 1px solid #87AFB5;
+  border-radius: 4px;
+  line-height: 2;
+}
+
+.booking-status {
+  margin-bottom: 10px;
+}
+
+.container {
+  max-width: 300px;
+  margin-left: auto;
+  margin-right: auto;
+  background-color: #fff;
+}
+
+div.eta {
+  padding: 4px 10px 2px 10px;
+  display: inline-block;
+  position: relative;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  border-radius: 4px;
+  margin: 6px;
+  display: none;
+}
+
+.display-date {
+  font-size: 1.3em;
+}
+
+span.date {
+  font-size: 1.3em;
+  float: right;
+}
+
+div.date-and-id {
+  text-align: left;
+  margin-bottom: 4px;
+  background-color: #dedede;
+  background-image: linear-gradient(to bottom, #f5f5f5 0, #52A7B4 80%);
+  padding: 2px 10px 2px 10px;
+  border-bottom: 1px solid #ddd;
+}
+
+.ready-time {
+  display: inline-block;
+  display: none;
+}
+
+div.locations {
+  text-align: left;
+  padding-left: 4px;
+}
+
+.no-eta {
+  background-color: "lightblue";
+}
+</style>
--- js/pizza-tracker.vue
+++ js/pizza-tracker.vue
@@ -1,24 +1,8 @@
 <template>
   <div class="pizza-tracker">
-    <div class="booked future-step" data-text="">
-      <div class="straighten"><i class="fa fa-calendar fa-lg"></i>
-      </div>
-    </div>
-    <div class="scheduled future-step" data-text="">
-      <div class="straighten"><i class="fa fa-ticket fa-lg"></i>
-      </div>
-    </div>
-    <div class="trip-soon future-step" data-text="">
-      <div class="straighten"><i class="fa fa-road fa-lg"></i>
-      </div>
-    </div>
-    <div class="five-minutes future-step" data-text="">
-      <div class="straighten"><i class="fa fa-clock-o fa-lg"></i>
-      </div>
-    </div>
-    <div class="here-now future-step" data-text="">
-      <div class="straighten">\
-        <div class="bell"><i class="fa fa-bell-o fa-lg"></i></div>
+    <div v-for="(step, step_id) in steps" :class="[step_id, getSteps(step_id)]" :data-text="step.text">
+      <div class="straighten">
+        <i :class="['fa', 'fa-lg', 'fa-' + step.icon]"></i>
       </div>
     </div>
   </div>
@@ -27,12 +11,153 @@
 <script>
 export default {
   data: function() {
-        return {};
+    return {
+      'steps': {
+        'booked': { status: false, icon: 'calendar', text: 'Booked' },
+        'scheduled': { status: false, icon: 'ticket', text: 'Van Assigned' },
+        'trip_soon': { status: false, icon: 'road', text: '30 min. Away' },
+        'five_minutes': { status: false, icon: 'clock-o', text: '5 min. Away' },
+        'here_now': { status: false, icon: 'bell', text: 'Arriving Now!' }
+      }
+    };
+  },
+  props: ["completedSteps"],
+  methods: {
+    markStepComplete: function(step) {
+      this.steps[step] = true;
     },
-    methods: {
-        markStepComplete: function(step) {
-            
+    getSteps: function(step_id) {
+      var classes = [];
+      if (typeof (this.completedSteps[step_id]) !== 'undefined') {
+        classes = ['complete'];
+        if (step_id == 'here_now') {
+          classes.push('bell-shake');
         }
+      } else {
+        classes = ['future-step'];
+      }
+      return classes;
     }
+  },
 }
 </script>
+
+<style scoped>
+.pizza-tracker {
+  position: relative;
+  margin-bottom: 40px;
+}
+
+.pizza-tracker>div {
+  position: static;
+  bottom: 0;
+  width: auto;
+  background-color: #eee;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  display: inline-block;
+  padding: 8px 16px;
+  margin-left: 0;
+  margin-right: -1px;
+  transform: skewX(-20deg);
+  margin-bottom: 4px;
+  color: rgba(0, 0, 0, 0.3);
+}
+
+.pizza-tracker>div::after {
+  position: absolute;
+  box-sizing: border-box;
+  transform: skewX(20deg);
+  height: 12px;
+  font-size: 10px;
+  font-weight: 700;
+  content: '';
+  line-height: normal;
+  width: 100%;
+  padding-top: 4px;
+  border-top: 2px solid #ddd;
+  bottom: -18px;
+  left: 3px;
+}
+
+.pizza-tracker>div:nth-child(1) {
+  border-radius: 4px 0 0 4px;
+}
+
+.pizza-tracker>div:last-child {
+  border-radius: 0 4px 4px 0;
+}
+
+.straighten {
+  display: inline-block;
+  transform: skewX(20deg);
+}
+
+.pizza-tracker>.complete {
+  background-color: darkseagreen;
+  background-image: linear-gradient(to bottom, #C3DDD0 0, #86BAA1 100%);
+  box-shadow: 0 0 0px 1px #b2dba1;
+  color: #000;
+}
+
+.pizza-tracker>.complete::after {
+  position: absolute;
+  box-sizing: border-box;
+  transform: skewX(20deg);
+  height: 12px;
+  font-size: 10px;
+  font-weight: 700;
+  content: attr(data-text);
+  line-height: normal;
+  width: 100%;
+  padding-top: 4px;
+  border-top: 2px solid #444;
+  bottom: -18px;
+  left: 3px;
+}
+
+.more-details {
+  opacity: 0.6;
+  display: block;
+  background-color: #efefef;
+}
+
+.pizza-tracker>.working {
+  background-color: #cec;
+  background-image: linear-gradient(to bottom, #fcf8e3 0, #FFCF56 100%);
+}
+
+.pizza-tracker>.future-step {
+  background-image: linear-gradient(to bottom, #fcf8e3 0, #FFCF56 100%);
+}
+
+.status-text {
+  font-size: 10px;
+  font-weight: 700;
+}
+
+i {
+  display: block;
+}
+
+.bell {}
+
+.bell-shake {
+  display: block;
+  animation: ring 1s infinite alternate;
+}
+
+@keyframes ring {
+  0% {
+    transform: rotate(-10deg);
+  }
+  33% {
+    transform: rotate(10deg);
+  }
+  66% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(-10deg);
+  }
+}
+</style>
